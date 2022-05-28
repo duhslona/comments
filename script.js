cf3f860fdcd25f022cc8406c6656f7ff921b7308
olgakopina <olga.kopina@ringcentral.com>
2022-05-29 00:18:56 +0400

    add comments with filter

---
+++ script.js
@@ -0,0 +1,47 @@
+const newCommentField = document.getElementById("text-area");
+const commentsField = document.getElementById("comments");
+
+function addComment(){
+    const text = newCommentField.value;
+
+    if( validate(text)==true){
+        addCommentWithText(text);
+    }
+    clearInput();
+}
+
+function validate(){
+    if(removeNewLines(newCommentField.value).length==0){
+        alert("Комментарий должен содержать текст");
+        return false;
+    }
+
+    return true;
+}
+
+function addHtmlNewLines(text){
+    return text.replaceAll(/(?:\r\n|\r|\n)/g, '<br>');
+}
+
+function removeNewLines(text){
+    return text.replaceAll(/(?:\r\n|\r|\n)/g, "");
+}
+
+function clearInput(){
+    newCommentField.value="";
+}
+
+function maskSpam(text){
+    return text.replaceAll("viagra", "***").replaceAll("XXX", "***");
+}
+
+function addCommentWithText(text){
+    let newComment = document.createElement('p');
+    
+    newComment.innerHTML=maskSpam(addHtmlNewLines(text));
+    
+    newComment.classList.add("border-bottom");
+    newComment.classList.add("border-warning");
+
+    commentsField.appendChild(newComment);
+}
