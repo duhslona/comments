5ab67236a14c3de60cac06b8b9b0151ae0c2be56
olgakopina <olga.kopina@ringcentral.com>
2022-06-12 23:56:15 +0400

    add local storage for userName and avatar

--- script.js
+++ script.js
@@ -1,11 +1,46 @@
 const newCommentField = document.getElementById("text-area");
 const commentsField = document.getElementById("comments");
+const userNameField = document.getElementById("userName");
+const avatarField = document.getElementById("userPic");
+const saveUserDataCheckbox = document.getElementById("saveUserData");
+
+const USER_NAME_ITEM="st.userName";
+const USER_AVATAR_ITEM="st.userAvatarLink";
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    if(localStorage.getItem(USER_NAME_ITEM)){
+        userNameField.value=localStorage.getItem(USER_NAME_ITEM);
+    }
+    if(localStorage.getItem(USER_AVATAR_ITEM)){
+        avatarField.value=localStorage.getItem(USER_AVATAR_ITEM);
+    }
+  });
+
+
+function saveUserDataIfNeeded(save, userName, avatarLink){
+    if(save){
+        if(userNameField.value!=''){
+            saveItemInLocalStorage(USER_NAME_ITEM, userName);
+        }
+        if(avatarField.value!=''){
+            saveItemInLocalStorage(USER_AVATAR_ITEM, avatarLink);
+        }
+    } else {
+        localStorage.removeItem(USER_NAME_ITEM);
+        localStorage.removeItem(USER_AVATAR_ITEM);
+    }
+}
 
 function addComment(){
     const text = newCommentField.value;
+    const userName = userNameField.value;
+    const avatarLink = avatarField.value;
+    const saveUserdata = saveUserDataCheckbox.checked;
 
     if( validate(text)==true){
-        addCommentWithText(text);
+        saveUserDataIfNeeded(saveUserdata, userName, avatarLink)
+        addCommentWithUserInfo(text, userName, avatarLink);
     }
     clearInput();
 }
@@ -35,7 +70,22 @@ function maskSpam(text){
     return text.replaceAll("viagra", "***").replaceAll("XXX", "***");
 }
 
-function addCommentWithText(text){
+function addCommentWithUserInfo(text, userNameValue, avatarLink){
+    let userContainer = document.createElement('div');
+    userContainer.setAttribute('style', 'display:flex; align-items:center');
+
+    let userPic = document.createElement('img');
+    userPic.setAttribute('width', '32');
+    userPic.setAttribute('height', '32');
+    userPic.setAttribute('src', avatarLink===''?'/anon.jpg':avatarLink);
+
+    let userName = document.createElement('p');
+    userName.setAttribute('style', 'margin-left:15px');
+    userName.innerHTML=userNameValue===''?'Anonymous':userNameValue;
+
+    userContainer.appendChild(userPic);
+    userContainer.appendChild(userName);
+
     let newComment = document.createElement('p');
     
     newComment.innerHTML=maskSpam(addHtmlNewLines(text));
@@ -43,5 +93,15 @@ function addCommentWithText(text){
     newComment.classList.add("border-bottom");
     newComment.classList.add("border-warning");
 
+    commentsField.appendChild(userContainer);
     commentsField.appendChild(newComment);
+}
+
+function saveItemInLocalStorage(itemName, itemValue){
+    if(localStorage.getItem(itemName)!=null&&localStorage.getItem(itemName)!=itemValue){
+        localStorage.removeItem(itemName);
+    } 
+    if(localStorage.getItem(itemName)!=itemValue){
+        localStorage.setItem(itemName, itemValue);
+    }
 }
